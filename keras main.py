5a9cfc0715b10d301a16bb1c7d0e33240a0ed7b6
Aniket <35391308+yoursDaddy007@users.noreply.github.com>
2021-10-08 14:02:00 +0530

    Update keras main.py

--- keras main.py
+++ keras main.py
@@ -22,7 +22,7 @@ cap = cv2.VideoCapture(0)
 while True:
     _, frame = cap.read()
     labels = []
-    gray = cv2.(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale
 
     for (x,y,w,h) in faces:
